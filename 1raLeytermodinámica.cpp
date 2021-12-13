dfdc277222513429f4f5f838f949182568bd7834
ComputerCrystal <95935625+ComputerCrystal@users.noreply.github.com>
2021-12-13 09:49:36 -0600

    Update 1raLeytermodinámica.cpp

--- 1raLeytermodinámica.cpp
+++ 1raLeytermodinámica.cpp
@@ -18,10 +18,10 @@ int main() {
 		"\n3.- Isocórico"
 		"\n4.- Isotérmico";
 	cout << "\nQue proceso desea calcular?: "; cin >> proceso;
-	 if (proceso == "Normal" or proceso == "normal") {
+	 if ((proceso == "Normal")||(proceso == "normal")) {
 		cout << "\nDigite el valor del calor(+ si absorbe o - si cede) en calorias: "; cin >> Q;
 		cout << "Digite el valor del trabajo termódinamico(+si realiza o - si le realizan): "; cin >> W;
-		if (Q == 0 or W == 0) {
+		if ((Q == 0)||(W == 0)) {
 			cout << "Error, datos insuficientes" << endl;
 			exit(1);
 		}
@@ -30,7 +30,7 @@ int main() {
 			cout << "\nEl cambio en la energía interna es: " << VI << endl;
 		}
 	}
-	else if (proceso == "Adiabatico" or proceso == "adiabatico") {
+	else if ((proceso == "Adiabatico")||(proceso == "adiabatico")) {
 		cout << "El sistema no recibe o cede calor (Q=0)";
 		cout << "\nDigite el valor del trabajo termodinámico: "; cin >> W;
 		VI = -W;
@@ -38,14 +38,14 @@ int main() {
 		cout << "\nEl cambio en la energía interna es de: " << VI << endl;
 		cout << "\nEl valor del trabajo termodinámico es de: " << W << endl;
 	}
-	else if (proceso == "Isocorico" or proceso == "isocorico") {
+	else if ((proceso == "Isocorico")||(proceso == "isocorico")) {
 		cout << "\nEl volumen del sistema es constante (W=0)";
 		cout << "\nDigite el valor del calor: "; cin >> Q;
 		VI = Q;
 		cout << "\nEl cambio en la energía interna es de: " << VI << endl;
 		cout << "\nEl valor del calor es de: " << Q << endl;
 	}
-	else if (proceso == "Isotermico" or proceso == "isotermico") {
+	else if ((proceso == "Isotermico")||(proceso == "isotermico")) {
 		cout << "\nLa temperatura permanece constante (ΔU)";
 		cout << "\nDigite el valor del calor: "; cin >> Q;
 		W = Q;
